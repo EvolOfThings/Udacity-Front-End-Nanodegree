1439f18016c6eefca59c9eb0608539b958b36322
RishadYamnoor <rishad.yamnoor@gmail.com>
2017-02-22 17:32:26 +0530

    Added new element and js function to description called Puns.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ function myThing() {
     alert("You are gonna have to wait little longer to be AMAZED!!!");
     return myThing;
 }
+
+function myThingPuns() {
+    alert("I like angles... To a degree.");
+    return myThingPuns;
+}
