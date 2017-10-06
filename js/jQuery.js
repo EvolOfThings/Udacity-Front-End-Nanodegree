9c9cb639b37a2898ca89dbbfd14d3239fc18e502
RishadYamnoor <rishad.yamnoor@gmail.com>
2017-10-06 13:18:35 +0530

    on click function for see more

--- js/jQuery.js
+++ js/jQuery.js
@@ -8,3 +8,15 @@ $('#education').hover(
   }
 );
 
+
+
+$(function(){ /* to make sure the script runs after page load */
+
+    $('a.see_more').click(function(event){ /* find all a.read_more elements and bind the following code to them */
+
+        event.preventDefault(); /* prevent the a from changing the url */
+        $(this).hide(); /* hide the read more button */
+        $(this).parents('.item').find('.more_projects').show(); /* show the .more_projects span */
+    });
+
+});
