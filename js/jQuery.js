c3835b1b38383bfe7dab13607270322275a98086
RishadYamnoor <rishad.yamnoor@gmail.com>
2017-10-20 20:42:10 +0530

    disabled see more

--- js/jQuery.js
+++ js/jQuery.js
@@ -26,13 +26,13 @@ $('#education').hover(
 
 // To see more projects
 
-$(function(){ /* to make sure the script runs after page load */
+// $(function(){ /* to make sure the script runs after page load */
 
-    $('a.see_more').click(function(event){ /* find all a.read_more elements and bind the following code to them */
+//     $('a.see_more').click(function(event){ /* find all a.read_more elements and bind the following code to them */
 
-        event.preventDefault(); /* prevent the a from changing the url */
-        $(this).hide(); /* hide the read more button */
-        $(this).parents('.item').find('.more_projects').show(); /* show the .more_projects span */
-    });
+//         event.preventDefault(); /* prevent the a from changing the url */
+//         $(this).hide(); /* hide the read more button */
+//         $(this).parents('.item').find('.more_projects').show(); /* show the .more_projects span */
+//     });
 
-});
+// });
